aacede3f62eff062d2930a72b8ae1be16c6740fb
seongmin <tjdalsdl0219@gmail.com>
2021-07-10 10:39:13 +0900

    특정 레이어에서 특성 추출 모델 생성

--- Sequential.py
+++ Sequential.py
@@ -71,3 +71,110 @@ if __name__ == '__main__':
     x = tf.ones((1, 4))
     y = model(x)
     print("Number of weights after calling the model:", len(model.weights))  # 6
+
+    model.summary()
+
+
+    # 현재 출력 포함하여 지금까지 모델 요약을 표시 할 때
+    #
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(4,)))
+    model.add(layers.Dense(2, activation="relu"))
+
+    model.summary()
+
+
+    # input 객체는 레이어가 아니므로 model.layers의 일부로 표시 되지 않는다
+    model.layers
+
+    # 입력 형상을 미리 입력하면 가중치가 존재하기 때문에 model 정보를 볼 수 있다
+
+    model = keras.Sequential()
+    model.add(layers.Dense(2, activation="relu", input_shape=(4,)))
+
+    model.summary()
+
+    # symetric matrix?
+
+    # tensordot
+
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(250, 250, 3))) # RGB images
+    model.add(layers.Conv2D(32, 5 , strides=2, activation="relu"))
+    model.add(layers.Conv2D(32, 3, activation="relu"))
+    model.add(layers.MaxPooling2D(3))
+
+    # Can you guess what the current output shape is at this point? Probably not.
+    # Let's just print it:
+    model.summary()
+    # maybe fail
+
+    # The answer was: (40, 40, 32), so we can keep downsampling...
+
+
+    model.add(layers.Conv2D(32, 3, activation="relu"))
+    model.add(layers.Conv2D(32, 3, activation="relu"))
+    model.add(layers.MaxPooling2D(3))
+    model.add(layers.Conv2D(32, 3, activation="relu"))
+    model.add(layers.Conv2D(32, 3, activation="relu"))
+    model.add(layers.MaxPooling2D(2))
+
+    # And now?
+    model.summary()
+
+    # Now that we have 4x4 feature maps, time to apply global max pooling.
+    model.add(layers.GlobalMaxPooling2D())
+
+    # Finally, we add a classification layer.
+    model.add(layers.Dense(10))
+
+    initial_model = keras.Sequential(
+        [
+            keras.Input(shape=(250, 250, 3)),
+            layers.Conv2D(32, 5, strides=2, activation="relu"),
+            layers.Conv2D(32, 3, activation="relu"),
+            layers.Conv2D(32, 3, activation="relu"),
+        ]
+    )
+    # 모든 중간 레이어의 출력을 추출하는 모델 생성
+    feature_extractor = keras.Model(
+
+        inputs=initial_model.inputs,
+        outputs=[layer.output for layer in initial_model.layers],
+
+
+    )
+
+    # Call feature extractor on test input.
+    x = tf.ones((1, 250, 250, 3))
+    features = feature_extractor(x)
+
+
+
+    # 하나의 레이어에서 특성만 추출하는 것과 유사
+    initial_model = keras.Sequential(
+        [
+            keras.Input(shape=(250, 250, 3)),
+            layers.Conv2D(32, 5, strides=2, activation="relu"),
+            layers.Conv2D(32, 3, activation="relu", name="my_intermediate_layer"),
+            layers.Conv2D(32, 3, activation="relu"),
+        ]
+    )
+
+    feature_extractor = keras.Model(
+        inputs=initial_model.inputs,
+        outputs=initial_model.get_layer(name="my_intermediate_layer").output,
+    )
+    # Call feature extractor on test input.
+    x = tf.ones((1, 250, 250, 3))
+    features = feature_extractor(x)
+
+
+    """ 전이 학습 """
+    # 가장 마지막 output layer를 동결하고 맨 위 레이어만 훈련
+
+    # model = keras.Sequential(
+    #     [ < br > keras.Input(shape=(784)) < br > layers.Dense(32, activation='relu'), < br > layers.Dense(32,
+    #                                                                                                       activation='relu'), < br > layers.Dense(
+    #     32, activation='relu'), < br > layers.Dense(
+    #     10), < br >]) < br > < br >  # Presumably you would want to first load pre-trained weights.<br>model.load_weights(...)<br> <br># Freeze all layers except the last one.<br>for layer in model.layers[:-1]:<br>  layer.trainable = False<br> <br># Recompile and train (this will only update the weights of the last layer).<br>model.compile(...)<br>model.fit(...)
